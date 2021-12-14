7469183ea053dd743244bf29c5ec1f3584676c59
GenArrow <edimociran@gmail.com>
2021-12-14 14:06:47 +0200

    Set default windows form size 1200 x 800 + Sizable set to False

--- COBRA-VestotoriiPrivamerii/Form1.Designer.cs
+++ COBRA-VestotoriiPrivamerii/Form1.Designer.cs
@@ -29,10 +29,20 @@ namespace COBRA_VestotoriiPrivamerii
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1184, 761);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
