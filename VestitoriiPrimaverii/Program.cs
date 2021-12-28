2c4b4b04a65c19e026cb6fc9774cbf780edb59fe
oanamatei21 <oana_mariamatei@yahoo.com>
2021-12-28 22:35:07 +0200

    joc Mediu Terminat

--- VestitoriiPrimaverii/Program.cs
+++ VestitoriiPrimaverii/Program.cs
@@ -16,7 +16,7 @@ namespace VestitoriiPrimaverii
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new JocMate1());
+            Application.Run(new JocMediuReguli());
         }
     }
 }
