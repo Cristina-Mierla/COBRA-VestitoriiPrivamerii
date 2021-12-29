0be43171cf515bd9bcc698e09d998447c551dee8
GenArrow <edimociran@gmail.com>
2021-12-29 14:36:18 +0200

    Joc romana, take 2

---
+++ VestitoriiPrimaverii/JocRomana1.Designer.cs
@@ -0,0 +1,208 @@
+﻿
+namespace COBRA_VestotoriiPrivamerii
+{
+    partial class JocRomana1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(JocRomana1));
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.SadLalea = new System.Windows.Forms.PictureBox();
+            this.SadZambila = new System.Windows.Forms.PictureBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.Zambila = new System.Windows.Forms.PictureBox();
+            this.Ghiocel = new System.Windows.Forms.PictureBox();
+            this.Lalea = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.ReadFairy = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.SadLalea)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SadZambila)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Zambila)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ghiocel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lalea)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ReadFairy)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SadLalea
+            // 
+            this.SadLalea.BackColor = System.Drawing.Color.Transparent;
+            this.SadLalea.Image = ((System.Drawing.Image)(resources.GetObject("SadLalea.Image")));
+            this.SadLalea.Location = new System.Drawing.Point(908, 50);
+            this.SadLalea.Name = "SadLalea";
+            this.SadLalea.Size = new System.Drawing.Size(131, 132);
+            this.SadLalea.TabIndex = 19;
+            this.SadLalea.TabStop = false;
+            this.SadLalea.Visible = false;
+            // 
+            // SadZambila
+            // 
+            this.SadZambila.BackColor = System.Drawing.Color.Transparent;
+            this.SadZambila.Image = ((System.Drawing.Image)(resources.GetObject("SadZambila.Image")));
+            this.SadZambila.Location = new System.Drawing.Point(908, 568);
+            this.SadZambila.Name = "SadZambila";
+            this.SadZambila.Size = new System.Drawing.Size(131, 132);
+            this.SadZambila.TabIndex = 18;
+            this.SadZambila.TabStop = false;
+            this.SadZambila.Visible = false;
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.BackColor = System.Drawing.Color.White;
+            this.richTextBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox4.Font = new System.Drawing.Font("Segoe UI", 20F);
+            this.richTextBox4.Location = new System.Drawing.Point(611, 606);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.ReadOnly = true;
+            this.richTextBox4.Size = new System.Drawing.Size(119, 74);
+            this.richTextBox4.TabIndex = 17;
+            this.richTextBox4.Text = "Zambila";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.BackColor = System.Drawing.Color.White;
+            this.richTextBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox3.Font = new System.Drawing.Font("Segoe UI", 20F);
+            this.richTextBox3.Location = new System.Drawing.Point(594, 354);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.ReadOnly = true;
+            this.richTextBox3.Size = new System.Drawing.Size(136, 54);
+            this.richTextBox3.TabIndex = 16;
+            this.richTextBox3.Text = "Ghiocelul";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.SystemColors.Window;
+            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox2.Font = new System.Drawing.Font("Segoe UI", 20F);
+            this.richTextBox2.Location = new System.Drawing.Point(605, 82);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(111, 54);
+            this.richTextBox2.TabIndex = 15;
+            this.richTextBox2.Text = "Laleaua";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // Zambila
+            // 
+            this.Zambila.BackColor = System.Drawing.Color.Transparent;
+            this.Zambila.Image = ((System.Drawing.Image)(resources.GetObject("Zambila.Image")));
+            this.Zambila.Location = new System.Drawing.Point(741, 534);
+            this.Zambila.Name = "Zambila";
+            this.Zambila.Size = new System.Drawing.Size(139, 195);
+            this.Zambila.TabIndex = 13;
+            this.Zambila.TabStop = false;
+            this.Zambila.Click += new System.EventHandler(this.Zambila_Click);
+            // 
+            // Ghiocel
+            // 
+            this.Ghiocel.BackColor = System.Drawing.Color.Transparent;
+            this.Ghiocel.Image = ((System.Drawing.Image)(resources.GetObject("Ghiocel.Image")));
+            this.Ghiocel.Location = new System.Drawing.Point(736, 269);
+            this.Ghiocel.Name = "Ghiocel";
+            this.Ghiocel.Size = new System.Drawing.Size(186, 234);
+            this.Ghiocel.TabIndex = 14;
+            this.Ghiocel.TabStop = false;
+            this.Ghiocel.Click += new System.EventHandler(this.Ghiocel_Click);
+            // 
+            // Lalea
+            // 
+            this.Lalea.BackColor = System.Drawing.Color.Transparent;
+            this.Lalea.Image = ((System.Drawing.Image)(resources.GetObject("Lalea.Image")));
+            this.Lalea.Location = new System.Drawing.Point(733, 3);
+            this.Lalea.Name = "Lalea";
+            this.Lalea.Size = new System.Drawing.Size(159, 227);
+            this.Lalea.TabIndex = 12;
+            this.Lalea.TabStop = false;
+            this.Lalea.Click += new System.EventHandler(this.Lalea_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Segoe UI", 15F);
+            this.richTextBox1.Location = new System.Drawing.Point(115, 26);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(215, 54);
+            this.richTextBox1.TabIndex = 11;
+            this.richTextBox1.Text = "Sa auzim ghicitoarea Click pe mine";
+            // 
+            // ReadFairy
+            // 
+            this.ReadFairy.BackColor = System.Drawing.Color.Transparent;
+            this.ReadFairy.Image = ((System.Drawing.Image)(resources.GetObject("ReadFairy.Image")));
+            this.ReadFairy.Location = new System.Drawing.Point(93, 85);
+            this.ReadFairy.Name = "ReadFairy";
+            this.ReadFairy.Size = new System.Drawing.Size(252, 370);
+            this.ReadFairy.TabIndex = 10;
+            this.ReadFairy.TabStop = false;
+            this.ReadFairy.Click += new System.EventHandler(this.ReadFairy_Click);
+            // 
+            // JocRomana1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1184, 761);
+            this.Controls.Add(this.SadLalea);
+            this.Controls.Add(this.SadZambila);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.Zambila);
+            this.Controls.Add(this.Ghiocel);
+            this.Controls.Add(this.Lalea);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.ReadFairy);
+            this.Name = "JocRomana1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "JocRomana1";
+            ((System.ComponentModel.ISupportInitialize)(this.SadLalea)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SadZambila)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Zambila)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ghiocel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lalea)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ReadFairy)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.PictureBox SadLalea;
+        private System.Windows.Forms.PictureBox SadZambila;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.PictureBox Zambila;
+        private System.Windows.Forms.PictureBox Ghiocel;
+        private System.Windows.Forms.PictureBox Lalea;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox ReadFairy;
+    }
+}
